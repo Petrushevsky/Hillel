67a00f7dc961315c19b1b3e205ca19e7c2983e5e
Eugene <odessa.eugene@gmail.com>
2022-02-01 17:41:02 +0200

    2021-2022

---
+++ src/Hillel/javaIntroduction/HomeWork28CoffeMachine/Americano.java
@@ -0,0 +1,13 @@
+package Hillel.javaIntroduction.HomeWork28CoffeMachine;
+
+public class Americano extends Drinks{
+    private int price = 35;
+
+    @Override
+    public void create() {
+        System.out.println("Americano created! price - " + price +"$");
+    }
+    public int getPrice(){
+        return price;
+    }
+}
---
+++ src/Hillel/javaIntroduction/HomeWork28CoffeMachine/Drinks.java
@@ -0,0 +1,12 @@
+package Hillel.javaIntroduction.HomeWork28CoffeMachine;
+
+public abstract class Drinks {
+
+    public Drinks(){}
+    //public void create(){}
+    public abstract void create();
+    public abstract int getPrice();
+}
+
+
+
